b121b0c12f0ba77a09f82996d5c04137cd530a43
Abner Freitas <defreitasabner@gmail.com>
2024-01-31 12:05:25 -0300

    Implemented search for controller method to execute request.

--- src/main.py
+++ src/main.py
@@ -2,9 +2,15 @@ from injectors.controller_injector import ControllerInjector
 
 
 def main():
-    subpath = 'example'
-    controller = ControllerInjector.search(subpath)
-    controller.response()
+    path = '/example/response'
+    subpaths = path.replace('/', '', 1).split('/')
+    controller = ControllerInjector.search(subpaths[0])
+    allowed_members = filter(lambda member: not member.startswith('__') and callable(getattr(controller, member)), dir(controller))
+    if subpaths[1] in allowed_members:
+        method = getattr(controller, subpaths[1])
+        method()
+    else:
+        raise Exception('Method not found')
 
 if __name__ == '__main__':
     main()
