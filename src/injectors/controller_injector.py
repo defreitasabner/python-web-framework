d6aaa481e3248f3256c05bc52afa86b22aec8255
Abner Freitas <defreitasabner@gmail.com>
2024-01-31 11:39:23 -0300

    Added exception if not find any controller.

--- src/injectors/controller_injector.py
+++ src/injectors/controller_injector.py
@@ -2,16 +2,19 @@ from pydoc import locate
 import inspect
 
 from injectors.injector import Injector
-
+from controllers.base_controller import BaseController
 
 class ControllerInjector(Injector):
     @staticmethod
     @Injector.auto_inject
-    def search(subpath):
+    def search(subpath) -> BaseController:
         package_name = 'controllers'
         package = locate(package_name)
         all_modules = inspect.getmembers(package, inspect.ismodule)
         controller_module = list(filter(lambda module: True if module[0].startswith(subpath) else False, all_modules))[0][1]
         all_classes = inspect.getmembers(controller_module, inspect.isclass)
         controller = list(filter(lambda cls: True if cls[1].__module__ == controller_module.__name__ else False, all_classes))[0][1]
-        return controller
+        if controller:
+            return controller
+        else:
+            raise Exception('Controller not found')
